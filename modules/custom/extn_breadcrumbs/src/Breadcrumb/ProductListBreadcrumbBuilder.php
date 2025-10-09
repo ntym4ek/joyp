827cd267be082d56a68efbde4fc7717f6953029e
ntym4ek <mega_comp@mail.ru>
2025-10-09 13:54:57 +0300

    крошки для блога

--- modules/custom/extn_breadcrumbs/src/Breadcrumb/ProductListBreadcrumbBuilder.php
+++ modules/custom/extn_breadcrumbs/src/Breadcrumb/ProductListBreadcrumbBuilder.php
@@ -30,7 +30,14 @@ class ProductListBreadcrumbBuilder implements BreadcrumbBuilderInterface {
     $links = [];
     $links[] = Link::fromTextAndUrl(t('Home'), Url::fromRoute('<front>'));
 
-    $view_id = $route_match->getParameter('view_id');
+    if ($view_id = $route_match->getParameter('view_id')) {
+      if ($view_id == 'blog') {
+        $links[] = Link::fromTextAndUrl('Блог', Url::fromUserInput('/blog'));
+      }
+      if ($view_id == 'katalog') {
+        $links[] = Link::fromTextAndUrl('Каталог', Url::fromUserInput('/catalog'));
+      }
+    }
 
     if ($term = $route_match->getParameter('taxonomy_term')) {
       if ($view_id == 'taxonomy_term') {
@@ -40,7 +47,8 @@ class ProductListBreadcrumbBuilder implements BreadcrumbBuilderInterface {
         }
       }
 
-      $links[] = Link::fromTextAndUrl($term->label(), Url::fromRoute('<none>'));
+      $prefix = $view_id == 'blog' ? '#' : '';
+      $links[] = Link::fromTextAndUrl($prefix . $term->label(), Url::fromRoute('<none>'));
     }
 
     return $breadcrumb->setLinks($links);
