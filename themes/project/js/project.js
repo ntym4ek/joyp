49015de1609b1941393c15fd45872d159a84bc17
ntym4ek <mega_comp@mail.ru>
2025-10-13 15:43:37 +0300

    мобильная вёрстка фильтров в каталоге

--- themes/project/js/project.js
+++ themes/project/js/project.js
@@ -22,6 +22,51 @@
         }
       );
 
+      // Раскрытый фильтр в Каталоге.
+      once('filter-once', '.block-views-exposed-filter-blockkatalog-page-1', context).forEach(
+        (element) => {
+
+          // банкет только для мобильных
+          if ($(window).width() < settings.theme.page_offside_hide_width) {
+
+            // стили фильтра
+            $(element).addClass('mobile-exposed');
+
+            // добавить кнопки сортировки и фильтров
+            $(element).before(
+              '<div class="mobile-exposed-trigger">' +
+                '<div class="row border-b">' +
+                  '<div class="col-xs-5 col-md-5 border-r">' +
+                    '<div class="mobile-exposed--sort"></div>' +
+                  '</div>' +
+                  '<div class="col-xs-5 col-md-5">' +
+                    '<div class="mobile-exposed--filters"><i class="picon picon--filter"></i><div>Фильтры</div></div>' +
+                  '</div>' +
+                '</div>' +
+              '</div>');
+
+            $(element).prepend(
+              '<div class="mobile-exposed__header project-padding border-b">' +
+                '<div class="h4">Фильтры</div>' +
+                '<i class="picon picon--cross"></i>' +
+              '</div>'
+            );
+
+
+            // обработчик открытия фильтров
+            $('.mobile-exposed--filters').on("click", function () {
+              $(element).addClass('mobile-exposed--open');
+            });
+
+            // обработчик закрытия фильтров
+            $('.mobile-exposed__header i, .mobile-exposed .form-actions input').on("click", function () {
+              $(element).removeClass('mobile-exposed--open');
+            });
+
+          }
+        }
+      );
+
     },
   };
 
